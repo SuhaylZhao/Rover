c82abf742a30b834c1ab4478531359b6bf8d67ee
Suhayl <838119509@qq.com>
2018-10-17 15:23:43 +0800

    add main.ts

---
+++ main.ts
@@ -0,0 +1,16 @@
+//% color="#AA278D"
+namespace basic {
+
+    //% block
+    export function helloWorld() {
+
+    }
+
+    // note that Caml casing yields lower case
+    // block text with spaces
+
+    //% block
+    export function camlCaseTwo() {
+
+    }
+}
