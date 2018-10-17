911dcf79d5e5349b42b9e14186697265495b9487
Suhayl <838119509@qq.com>
2018-10-17 17:17:54 +0800

    example
    example

--- main.ts
+++ main.ts
@@ -1,16 +1,3 @@
-//% color="#AA278D"
-namespace basic {
-
-    //% block
-    export function helloWorld() {
-
-    }
-
-    // note that Caml casing yields lower case
-    // block text with spaces
-
-    //% block
-    export function camlCaseTwo() {
-
-    }
-}
+basic.forever(() => {
+	
+})
