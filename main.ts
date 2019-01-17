3960909a8006e652b2fdf13e00847bbbcca6fbfb
Suhayl <838119509@qq.com>
2019-01-17 13:53:38 +0800

    update.
    update.

--- main.ts
+++ main.ts
@@ -23,6 +23,20 @@ enum RoverColors {
     //% block=black
     Black = 0x000000
 }
+enum Orders {
+    MOVE = 0,
+    STOP = 1,
+    ORDER_RGB = 2,
+    BUZZER = 3,
+    DISTANCE = 4,
+    LIGHTING = 5,
+    TRACKING = 6,
+    MODE = 7,
+    VOLTAGE = 8,
+    ECHO_OK = 9,
+    NONE = 10
+}
+
 //% color="#31C7D5" weight=10 icon="\uf1b9"
 namespace Rover {
     const PCA9685_ADDRESS = 0x43
@@ -44,31 +58,51 @@ namespace Rover {
     const TRIG_PIN = DigitalPin.P12
     const ECHO_PIN = DigitalPin.P13
 
-    enum RGBLED1 {
-        R = 14,
-        G = 15,
-        B = 13
+    class RGB {
+        redPin: number;
+        greenPin: number;
+        bluePin: number;
+        color: number;
+
+        constructor(_redPin: number, _greenPin: number, _bluePin: number) {
+            this.redPin = _redPin;
+            this.greenPin = _greenPin;
+            this.bluePin = _bluePin;
+            this.color = 0;
+        }
+
+        setColor(ccolor: number): void {
+            if (!initialized) {
+                initPCA9685()
+            }
+            this.color = ccolor;
+            let blue = ccolor & 0xFF;
+            let green = ccolor >> 8 & 0xFF;
+            let red = ccolor >> 16 & 0xFF;
+            if (brightness < 255) {
+                red = red * brightness >> 8;
+                green = green * brightness >> 8;
+                blue = blue * brightness >> 8;
+            }
+            setPwm(this.redPin, 0, red * 16)
+            setPwm(this.greenPin, 0, green * 16)
+            setPwm(this.bluePin, 0, blue * 16)
+        }
+        refresh(): void {
+            this.setColor(this.color)
+        }
     }
-    enum RGBLED2 {
-        R = 5,
-        G = 6,
-        B = 4
-    }
-    enum RGBLED3 {
-        R = 8,
-        G = 9,
-        B = 7
-    }
-    enum RGBLED4 {
-        R = 11,
-        G = 12,
-        B = 10
-    }
+
+    let leds1 = new RGB(14, 15, 13);
+    let leds2 = new RGB(5, 6, 4);
+    let leds3 = new RGB(8, 9, 7);
+    let leds4 = new RGB(11, 12, 10);
+
     export enum RGBLED {
-        RGBLED1 = 1,
-        RGBLED2 = 2,
-        RGBLED3 = 3,
-        RGBLED4 = 4
+        LED1 = 1,
+        LED2 = 2,
+        LED3 = 3,
+        LED4 = 4
     }
 
     export enum Motors {
@@ -76,11 +110,13 @@ namespace Rover {
         M2 = 0x2
     }
 
-    let initialized = false
-    let initializedMatrix = false
-    let matBuf = pins.createBuffer(17);
+    let ordersAyyay = "ABCDEFGHIJK";
+    let initialized = false;
     let distanceBuf = 0;
     let brightness = 255;
+    let orderString = "";
+    let currentOrder = "K";
+    let parameterList: string[] = []
     function i2cwrite(addr: number, reg: number, value: number) {
         let buf = pins.createBuffer(2)
         buf[0] = reg
@@ -142,12 +178,16 @@ namespace Rover {
         setPwm((index - 1) * 2, 0, 0);
         setPwm((index - 1) * 2 + 1, 0, 0);
     }
-    //% blockId=rover_setBrightness block="Brightness %br"
+    //% blockId=rover_setBrightness block="Set Brightness to%br"
     //% weight=200
     //% br.min=0 br.max=255
     //%br.defl=255
     export function setBrightness(br: number): void {
         brightness = br;
+        leds1.refresh();
+        leds2.refresh();
+        leds3.refresh();
+        leds4.refresh();
     }
     //% blockId=rover_setRGBLED block="Set %index Show %ccolor=rover_colors"
     //% weight=195
@@ -155,34 +195,18 @@ namespace Rover {
         if (!initialized) {
             initPCA9685()
         }
-        let blue = ccolor & 0xFF;
-        let green = ccolor >> 8 & 0xFF;
-        let red = ccolor >> 16 & 0xFF;
-        if (brightness < 255) {
-            red = red * brightness >> 8;
-            green = green * brightness >> 8;
-            blue = blue * brightness >> 8;
-        }
         switch (index) {
-            case RGBLED.RGBLED1:
-                setPwm(RGBLED1.R, 0, red * 16)
-                setPwm(RGBLED1.G, 0, green * 16)
-                setPwm(RGBLED1.B, 0, blue * 16)
+            case RGBLED.LED1:
+                leds1.setColor(ccolor);
                 break;
-            case RGBLED.RGBLED2:
-                setPwm(RGBLED2.R, 0, red * 16)
-                setPwm(RGBLED2.G, 0, green * 16)
-                setPwm(RGBLED2.B, 0, blue * 16)
+            case RGBLED.LED2:
+                leds2.setColor(ccolor);
                 break;
-            case RGBLED.RGBLED3:
-                setPwm(RGBLED3.R, 0, red * 16)
-                setPwm(RGBLED3.G, 0, green * 16)
-                setPwm(RGBLED3.B, 0, blue * 16)
+            case RGBLED.LED3:
+                leds3.setColor(ccolor);
                 break;
-            case RGBLED.RGBLED4:
-                setPwm(RGBLED4.R, 0, red * 16)
-                setPwm(RGBLED4.G, 0, green * 16)
-                setPwm(RGBLED4.B, 0, blue * 16)
+            case RGBLED.LED4:
+                leds4.setColor(ccolor);
                 break;
             default:
                 break;
@@ -203,27 +227,14 @@ namespace Rover {
     */
     //% blockId="rover_colors" block="Color %color"
     //% weight=185  
+    //% advanced=true
     export function colors(color: RoverColors): number {
         return color;
     }
     //% block="color $color"
     //% color.shadow="colorNumberPicker"
-    //% weight=180
+    //% weight=180    
     export function showColor(color: number): number {
-        return color;
-    }
-
-    //% block="color wheel $color"
-    //% color.shadow="colorWheelPicker"
-    //% weight=175
-    export function showColorWheel(color: number): number {
-        return color;
-    }
-
-    //% block="color wheel hsv $color"
-    //% color.shadow="colorWheelHsvPicker"
-    //% weight=170
-    export function showColorWheelHsv(color: number): number {
         return color;
     }
     //% blockId=rover_rgb block="red%red | green%green | blue%blue"
@@ -234,17 +245,67 @@ namespace Rover {
     //% red.defl=128
     //% green.defl=128
     //% blue.defl=128
+    //% advanced=true
     export function rgb(red: number, green: number, blue: number): number {
         return packRGB(red, green, blue);
     }
     function packRGB(a: number, b: number, c: number): number {
         return ((a & 0xFF) << 16) | ((b & 0xFF) << 8) | (c & 0xFF);
     }
+    /**
+     * Converts a hue saturation luminosity value into a RGB color
+     * @param h hue from 0 to 360
+     * @param s saturation from 0 to 99
+     * @param l luminosity from 0 to 99
+     */
+    //% blockId=rover_HSL block="hue %h|saturation %s|luminosity %l"
+    //% weight=164
+    //% h.min=0 h.max=360
+    //% s.min=0 s.max=99
+    //% l.min=0 l.max=99
+    //% h.defl=0
+    //% s.defl=99
+    //% l.defl=50
+    export function hsl(h: number, s: number, l: number): number {
+        h = Math.round(h);
+        s = Math.round(s);
+        l = Math.round(l);
 
+        h = h % 360;
+        s = Math.clamp(0, 99, s);
+        l = Math.clamp(0, 99, l);
+        let c = Math.idiv((((100 - Math.abs(2 * l - 100)) * s) << 8), 10000); //chroma, [0,255]
+        let h1 = Math.idiv(h, 60);//[0,6]
+        let h2 = Math.idiv((h - h1 * 60) * 256, 60);//[0,255]
+        let temp = Math.abs((((h1 % 2) << 8) + h2) - 256);
+        let x = (c * (256 - (temp))) >> 8;//[0,255], second largest component of this color
+        let r$: number;
+        let g$: number;
+        let b$: number;
+        if (h1 == 0) {
+            r$ = c; g$ = x; b$ = 0;
+        } else if (h1 == 1) {
+            r$ = x; g$ = c; b$ = 0;
+        } else if (h1 == 2) {
+            r$ = 0; g$ = c; b$ = x;
+        } else if (h1 == 3) {
+            r$ = 0; g$ = x; b$ = c;
+        } else if (h1 == 4) {
+            r$ = x; g$ = 0; b$ = c;
+        } else if (h1 == 5) {
+            r$ = c; g$ = 0; b$ = x;
+        }
+        let m = Math.idiv((Math.idiv((l * 2 << 8), 100) - c), 2);
+        let r = r$ + m;
+        let g = g$ + m;
+        let b = b$ + m;
+        return packRGB(r, g, b);
+    }
     //% blockId=rover_MotorRun block="Motor|%index|speed %speed"
     //% weight=80
     //% speed.min=-255 speed.max=255
     //% speed.defl=50
+    //% advanced=true
     export function MotorRun(index: Motors, speed: number): void {
         if (!initialized) {
             initPCA9685()
@@ -294,7 +355,6 @@ namespace Rover {
     //% blockId=rover_move_forward block="Move speed $speed"
     //% weight=95
     //% speed.min=-255 speed.max=255
-    //% speed.shadow="speedPicker"
     //% speed.defl=50
     export function Move(speed: number): void {
         MotorRunDual(speed, speed);
@@ -302,6 +362,7 @@ namespace Rover {
 
     //% blockId=rover_stop block="Motor Stop|%index|"
     //% weight=75
+    //% advanced=true
     export function MotorStop(index: Motors): void {
         MotorRun(index, 0);
     }
@@ -326,15 +387,15 @@ namespace Rover {
         control.waitMicros(10);
         pins.digitalWritePin(TRIG_PIN, 0)
 
-        // read pulse
-        let d = pins.pulseIn(ECHO_PIN, PulseValue.High, 25000);
+        // read pulse  max distance : 6m  
+        let d = pins.pulseIn(ECHO_PIN, PulseValue.High, 35000);
         let ret = d;
         // filter timeout spikes
         if (ret == 0 && distanceBuf != 0) {
             ret = distanceBuf;
         }
         distanceBuf = d;
-        return ret * 10 / 6 / 58;
+        return Math.round(ret * 10 / 6 / 58);
     }
     //% blockId=rover_line_tracking block="LineTracking"
     //% weight=65
@@ -350,11 +411,46 @@ namespace Rover {
     }
     //% blockId=rover_bettery_level block="Bettery_level"
     //% weight=60
+    //% advanced=true
     export function BatteryLevel(): number {
         let p2_adc = pins.analogReadPin(AnalogPin.P2);
         let bat_valotage = Math.round(p2_adc * 6.4516);     //unit: mV ,6.4516 = ~ 2*3.3/1023
         return bat_valotage;
     }
-
+    //% blockId=rover_set_receive_string block="Set Command to $receivedString"
+    //% weight=50
+    //% advanced=true
+    export function setReceiveString(receivedString: string): void {
+        parameterList = []
+        let interval = receivedString.indexOf("#")
+        while (interval >= 0) {
+            let parameter = receivedString.substr(0, interval)
+            parameterList.push(parameter)
+            receivedString = receivedString.substr(interval + 1, 50)
+            interval = receivedString.indexOf("#")
+        }
+        currentOrder = parameterList.shift()
+    }
+    //% blockId=rover_check_order block="Order is %_inOrder"
+    //% weight=50
+    //% advanced=true
+    export function checkOrder(_inOrder: Orders): boolean {
+        if (currentOrder == ordersAyyay[_inOrder])
+            return true
+        else
+            return false
+    }
+    //% blockId=rover_length_of_paramters block="Lenght of Paramters"
+    //% weight=50
+    //% advanced=true
+    export function parametersLength():number{
+        return parameterList.length;
+    }
+    //% blockId=rover_paramtersList block="ParamtersList"
+    //% weight=50
+    //% advanced=true
+    export function parametersList():string[]{
+        return parameterList;
+    }
 }
 
